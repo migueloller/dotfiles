b50367cd197306dd673638691a9ae231f231ab43
Miguel Oller <maoa22@gmail.com>
2023-12-06 10:50:19 -0500

    Add Git Credential Manager to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+brew install --cask git-credential-manager
 brew install nvim
 brew install google-cloud-sdk
 brew install fnm
