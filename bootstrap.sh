df3be2e3a2cc1059189450c06049c95d9a829909
Miguel Oller <maoa22@gmail.com>
2023-12-06 10:47:44 -0500

    Add nvim to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+brew install nvim
 brew install google-cloud-sdk
 brew install fnm
