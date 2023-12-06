763ac7a281e39e77053787a6edad632fd62cbc8b
Miguel Oller <maoa22@gmail.com>
2023-12-06 10:44:58 -0500

    Fix bootstrap home dir quoting

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 brew install google-cloud-sdk
 brew install fnm
